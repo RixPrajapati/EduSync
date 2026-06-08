5089a3f0c0a62df6fe8b3ac25d8c2e2f5c2cc7a8
Dipak Raj Pandey <dipakrajpandey31@gmail.com>
2026-06-08 21:54:04 +0545

    creating navigation

---
+++ navigation/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#faf9f9",
+        dark: "#1a1a1a",
+        light: "#4c4d4f",
+        accent: "#13c1fe",
+        primary: "#fe5013",
+      },
+    },
+  },
+  plugins: [],
+};
